117bf80ff28868ae8be86dc5cdd00b32be79de8a
Lê Hiền Lương <75094831+luongic@users.noreply.github.com>
2022-06-26 01:05:36 +0700

    renew view 3
    change view 3 display

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,24 +1,29 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+
 
 // Tab view 1 control
-const about = document.querySelector(".about");  
- const btns = document.querySelectorAll(".tab-btn");  
- const articles = document.querySelectorAll(".content");  
- about.addEventListener("click", function (e) {  
-  const id = e.target.dataset.id;  
-  if (id) {  
-   // remove selected from other buttons  
-   btns.forEach(function (btn) {  
-    btn.classList.remove("active");  
-   });  
-   e.target.classList.add("active");  
-   // hide other articles  
-   articles.forEach(function (article) {  
-    article.classList.remove("active");  
-   });  
-   const element = document.getElementById(id);  
-   element.classList.add("active");  
-  }  
- });  
+const about = $(".about");  
+const btns = $$(".tab-btn");  
+const articles = $$(".content");  
+
+about.addEventListener("click", function (e) {  
+    const id = e.target.dataset.id;  
+    if (id) {  
+        // remove selected from other buttons  
+        btns.forEach(function (btn) {  
+        btn.classList.remove("active");  
+    });  
+    e.target.classList.add("active");  
+        // hide other articles  
+        articles.forEach(function (article) {  
+        article.classList.remove("active");  
+    });  
+    const element = document.getElementById(id);  
+    element.classList.add("active");  
+    }  
+});  
 
 
 
@@ -31,7 +36,7 @@ const about = document.querySelector(".about");
                  
 let userAgent = navigator.userAgent;
 let browserName;
-const linkicon = document.querySelectorAll('.linkto__icon')
+const linkicon = $$('.linkto__icon')
 
 if(userAgent.match(/chrome|chromium|crios/i)){
     browserName = "chrome";
@@ -62,3 +67,123 @@ if(userAgent.match(/chrome|chromium|crios/i)){
     browserName="browser";
 }
 
+
+
+const listPro = $('.view3__content')
+
+
+const app = {
+    
+    projects: [
+        {
+            name: 'File manager',
+            url: '/assets/image/',
+            link: ' ',
+            no: 0
+        },
+        {
+            name: 'File manager',
+            url: '/assets/image/filemanager.png',
+            link: ' ',
+            no: 1
+        },
+        {
+            name: 'Game 3D',
+            url: '/assets/image/homecoming.jpg',
+            link: ' ',
+            no: 2
+        },
+        {
+            name: 'Clone UI Shopee',
+            url: '/assets/image/shopeeUI.png',
+            link: ' ',
+            no: 3
+        },
+        {
+            name: ' ',
+            url: '/assets/image/instagramUI.png',
+            link: ' ',
+            no: 4
+        },
+        {
+            name: ' ',
+            url: '/assets/image/musicplayer.png',
+            link: ' ',
+            no: 5
+        },
+        {
+            name: ' ',
+            url: '/assets/image/',
+            link: ' ',
+            no: 6
+        }
+    ],
+    render: function(url1, url2, url3){
+    
+        const htmls = `
+            <div class="item-container">
+                <div class="item prev-item" data-index="${url1}" onclick = leftClick() style="background-image: url(${this.projects[url1].url})"></div>
+                <div class="item curr-item" data-index="${url2}" style="background-image: url(${this.projects[url2].url})"></div>
+                <div class="item next-item" data-index="${url3}" onclick = rightClick() style="background-image: url(${this.projects[url3].url})"></div>
+            </div>
+            `
+        listPro.innerHTML = htmls
+    },
+
+    start: function() {
+        this.render(5, 1, 2)
+    }
+}
+
+app.start()
+
+const leftClick = function(){
+    const prev = $('.prev-item')
+    const prevItem = prev.dataset.index
+    console.log(prevItem)
+    prev.onclick = function(){
+        if (prevItem == 0){
+            app.render(5, 0, 1)
+        }
+        else if(prevItem == 1){
+            app.render(5, 1, 2)
+        }
+        else if(prevItem == 2){
+            app.render(1, 2, 3)
+        }
+        else if(prevItem == 3){
+            app.render(2, 3, 4)
+        }
+        else if(prevItem == 4){
+            app.render(3, 4, 5)
+        }
+        else{
+            app.render(4, 5, 1)
+        }
+    }
+}
+
+const rightClick = function(){
+    const next = $('.next-item')
+    const nextItem = next.dataset.index
+    next.onclick = function(){
+        if (nextItem == 1){
+            app.render(5, 1, 2)
+        }
+        else if(nextItem == 2){
+            app.render(1, 2, 3)
+        }
+        else if(nextItem == 3){
+            app.render(2, 3, 4)
+        }
+        else if(nextItem == 4){
+            app.render(3, 4, 5)
+        }
+        else if(nextItem == 5){
+            app.render(4, 5, 1)
+        }
+        else{
+
+        }
+    }
+}
